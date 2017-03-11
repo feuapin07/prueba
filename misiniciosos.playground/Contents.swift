83c4f032a7fef0411557087c426331fac806fc45
mac <macfolder@macs-Mac.local>
2017-03-11 13:31:14 -0800

    primera parte

---
+++ misiniciosos.playground/Contents.swift
@@ -0,0 +1,17 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var primerVariable : String = "hola mundo" //variable que se puede modificar en cualquier parte del codigo
+
+let primerVariableconstante :String = "hello word" //constante que solo almacena una valor
+
+//asignamos otra cadena de texto dentro de la variable primeraaVariable
+primerVariable = "se modifica esta variable"
+
+//imprinir en consola las variables y constantes que tenemos en nuestro proyecto
+print(primerVariable)
+print(primerVariableconstante)
+
